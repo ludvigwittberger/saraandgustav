3d978f395a51a10892ddcbc46ab8e65787b40020
ludvigakesson <ludvig.akesson@advisa.se>
2023-08-02 20:10:42 +0200

    Remove stuff

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,8 +4,8 @@ const Main = () => {
   return (
     <div className="u-margin--bottom u-padding--top-2">
       <h1 className="c-main__heading1">VI SKA GIFTA OSS!</h1>
-      <h1 className="c-main__heading">EMILIA <br /> & <br /> LUDVIG</h1>
-      <h3 className="c-main__date">19 juni 2021</h3>
+      <h1 className="c-main__heading">Sara <br /> & <br /> Gustav</h1>
+      <h3 className="c-main__date">17 augusti 2024</h3>
     </div>
   )
 };
